8bafa6640f062b9ac94e3230b88797e213c60672
Klesaer <hujunyi2008@gmail.com>
2026-08-18 11:47:07 +0200

    add player status

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,11 +9,18 @@ class_name Player
 @export var crit_chance: float = 0.0
 @export var crit_damage: float = 0.0
 
+@export_group()
+
 @onready var anim_sprite: AnimatedSprite2D = $AnimSprite
 @onready var fsm: FSM = $FSM
+@onready var health_component: HealthComponent = $HealthComponent
 
-
+var curr_mana: float 
 var last_direction: String = "down"
+
+func _ready() -> void:
+	health_component.setup(max_health)
+
 
 func _process(delta: float) -> void:
 	if fsm.curr_state:
@@ -38,9 +45,22 @@ func update_direction(input_vector: Vector2) -> void:
 func play_direction_anim(anim_name: String) -> void:
 	anim_sprite.play("%s_%s" % [anim_name, last_direction])
 	
+
+func step() -> void:
+	reset_health()
+	reset_mana()
+
+func reset_health() -> void:
+	health_component.setup(max_health)
+	EventBus.on_player_health_updated.emit(max_health, max_health)
 	
+
+func reset_mana() -> void:
+	curr_mana = max_mana
+	EventBus.on_player_health_updated.emit(max_mana, max_mana)
 	
-	
-	
-	
-	
+
+func use_mana(value: float) -> void:
+	curr_mana -= value
+	curr_mana = max(curr_mana, 0)
+	EventBus.on_player_health_updated.emit(curr_mana, max_mana)
