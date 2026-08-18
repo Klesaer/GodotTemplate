96fa50c8350debdea70f427914dcb163e077ccb0
Klesaer <hujunyi2008@gmail.com>
2026-08-18 11:19:15 +0200

    Signed-off-by: Klesaer <hujunyi2008@gmail.com>

--- scenes/player/states/player_state_idle.gd
+++ scenes/player/states/player_state_idle.gd
@@ -2,5 +2,9 @@ extends PlayerState
 class_name PlayStateIdle
 
 func enter_state() -> void:
-	pass
+	player.play_direction_anim("idle")
 	
+	
+func _input(event: InputEvent) -> void:
+	if player.is_moving():
+		fsm.transition_to("Walk")
